434f07a3547533342a1a2aec6bf094c30a953d19
unknown <juslepi@gmail.com>
2022-12-16 04:04:18 +0200

    get data from firestore

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,31 @@
-import Navbar from '../components/Navbar'
+import { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
+import PostBanner from "../components/PostBanner";
+import { getPosts } from "../util/firebase";
 
-import styles from '../styles/Home.module.css'
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const [posts, setPosts] = useState<any>([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await getPosts();
+
+      setPosts(res);
+    };
+
+    fetchPosts();
+  }, []);
+
   return (
-    <div >
+    <div className={styles.container}>
       <Navbar />
+      <ul>
+        {posts.map((post: any) => (
+          <li key={post.id}><PostBanner post={post}/></li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
