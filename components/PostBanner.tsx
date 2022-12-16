72a1ebdc5927dc985904e162d3132f14c7048679
unknown <juslepi@gmail.com>
2022-12-16 04:13:07 +0200

    postbanner init

---
+++ components/PostBanner.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import styles from "../styles/PostBanner.module.css";
+
+type PostBannerProps = {
+  post: any;
+};
+
+const PostBanner = ({ post }: PostBannerProps) => {
+  const { id, poster, title, timestamp } = post;
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <div className={styles.avatar}>{poster[0]}</div>
+      </div>
+      <div className={styles.body}>
+        <h6>{title}</h6>
+        <p>{poster}</p>
+      </div>
+      <p className={styles.timestamp}>Posted {timestamp}</p>
+    </div>
+  );
+};
+
+export default PostBanner;
