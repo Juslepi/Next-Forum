fbf279759188610917d10cd092cf64facf277912
unknown <juslepi@gmail.com>
2022-12-16 10:50:53 +0200

    styling

--- components/PostBanner.tsx
+++ components/PostBanner.tsx
@@ -10,14 +10,18 @@ const PostBanner = ({ post }: PostBannerProps) => {
   const { id, poster, title, timestamp } = post;
   return (
     <div className={styles.container}>
-      <div className={styles.header}>
-        <div className={styles.avatar}>{poster[0]}</div>
+      <div className={styles.left}>
+        <div className={styles.header}>
+          <div className={styles.avatar}>{poster[0]}</div>
+        </div>
+        <div className={styles.body}>
+          <h6>{title}</h6>
+          <p>{poster}</p>
+        </div>
       </div>
-      <div className={styles.body}>
-        <h6>{title}</h6>
-        <p>{poster}</p>
+      <div className={styles.right}>
+        {/* <p className={styles.timestamp}>Posted ago</p> */}
       </div>
-      {/* <p className={styles.timestamp}>Posted ago</p> */}
     </div>
   );
 };
