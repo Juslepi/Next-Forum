d4ce74cff4f79a4f4f02a5107d981168e3e237cf
unknown <juslepi@gmail.com>
2022-12-19 05:30:22 +0200

    removed imports

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,43 +1,43 @@
-import { useEffect, useState } from "react"
-import Router, { useRouter } from "next/router"
-import { getPosts } from "../../util/firebase"
+import { getPosts } from "../../util/firebase";
 import NewPostForm from "../../components/NewPostForm";
 
+import styles from "./Post.module.css";
+
 const Page = ({ post }: any) => {
-  const { title, poster, timestamp } = post
+  const { title, poster, timestamp, content } = post;
   return (
-    <div>
+    <div className={styles.container}>
       <h3>{title}</h3>
       <p>{poster}</p>
       <p>{timestamp}</p>
+      <p>{content || ""}</p>
       <NewPostForm formOpen={true} />
     </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
 
 export async function getStaticPaths(): Promise<any> {
   const posts = await getPosts();
   console.log(posts);
-  
+
   const paths = posts.map((post) => ({
     params: { id: post.id },
   }));
   return { paths, fallback: false };
 }
 
-export async function getStaticProps({params}: any): Promise<any> {
+export async function getStaticProps({ params }: any): Promise<any> {
   const posts = await getPosts();
   console.log(posts);
-  
+
   const { id } = params;
   const post = posts.find((p) => p.id === id);
-  
+
   return {
     props: {
-      post: post
+      post: post,
     },
   };
 }
-
