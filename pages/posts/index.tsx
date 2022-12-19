b63d27c1a637d45fc1b9057d925df15b5521a58b
unknown <juslepi@gmail.com>
2022-12-19 12:35:49 +0200

    Split posts into pages
    -Split posts into chunks
    -Render links for page choosing

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,25 +1,66 @@
 import { useEffect, useState } from "react";
 import Link from "next/link";
-import Navbar from "../../components/Navbar";
 import PostBanner from "../../components/PostBanner";
 import { getPosts } from "../../util/firebase";
 import NewPostForm from "../../components/NewPostForm";
 import styles from "../../styles/Home.module.css";
 
+const RESULTS_PER_PAGE = 5;
+
 export default function Home({ posts }: any) {
   const [formOpen, setFormOpen] = useState(false);
-  
+  const [pages, setPages] = useState(Array());
+  const [currentPage, setCurrentPage] = useState(0);
+
+  useEffect(() => {
+    // Split posts to multiple pages
+    const chunks = Array();
+    for (let i = 0; i < posts.length; i += RESULTS_PER_PAGE) {
+      const chunk = posts.slice(i, i + RESULTS_PER_PAGE);
+      chunks.push(chunk);
+    }
+    setPages(chunks);
+  }, [posts, setPages]);
+
+  // Returns values for navigation links
+  const navigationLinks = () => {
+    const rtrn = [];
+    const x = posts.length / RESULTS_PER_PAGE;
+
+    for (let i = 0; i < x; i++) {
+      rtrn.push(i);
+    }
+    return rtrn;
+  };
+
   return (
     <div className={styles.container}>
-      <ul>
-        {posts.map((post: any) => (
-          <Link href={`/posts/${post.id}`} key={post.id}>
-            <li>
-              <PostBanner post={post} />
-            </li>
-          </Link>
-        ))}
+      <ul className={styles.banner_container}>
+        {pages.length
+          ? pages[currentPage].map((post: any) => (
+              <Link href={`/posts/${post.id}`} key={post.id}>
+                <li>
+                  <PostBanner post={post} />
+                </li>
+              </Link>
+            ))
+          : ""}
       </ul>
+
+      {/* Render navigation links for pages */}
+      <div className={styles.link_row}>
+        {navigationLinks().map((link) => (
+          <button
+            key={link}
+            onClick={() => setCurrentPage(link)}
+            className={currentPage === link ? styles.active : ""}
+          >
+            {link + 1}
+          </button>
+        ))}
+      </div>
+
+      {/* New Post Button */}
       <div className={styles.toolbar}>
         <button
           className={formOpen ? "hidden" : ""}
@@ -28,6 +69,7 @@ export default function Home({ posts }: any) {
           New Post
         </button>
       </div>
+
       <div className={styles.form}>
         <NewPostForm formOpen={formOpen} setFormOpen={setFormOpen} />
       </div>
@@ -35,7 +77,7 @@ export default function Home({ posts }: any) {
   );
 }
 
-export async function getStaticProps(context: any): Promise<any> {
+export async function getStaticProps(): Promise<any> {
   return {
     props: {
       posts: await getPosts(),
